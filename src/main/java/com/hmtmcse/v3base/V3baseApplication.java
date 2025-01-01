ab7e7942b093b8461415d2536a3d34a9798ce6af
H.M.Touhid Mia (HMTMCSE) <hmtm.cse@gmail.com>
2025-01-01 10:50:43 +0600

    Bismillah

---
+++ src/main/java/com/hmtmcse/v3base/V3baseApplication.java
@@ -0,0 +1,13 @@
+package com.hmtmcse.v3base;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class V3baseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(V3baseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hmtmcse/v3base/V3baseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hmtmcse.v3base;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class V3baseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
